67c907062b2ca4972e6377d842a9c17021998741
Vedant09 <vedantvy98@gmail.com>
2024-01-19 23:09:06 -0500

    navbar

--- mhhrp/src/components/PropertyLists/index.jsx
+++ mhhrp/src/components/PropertyLists/index.jsx
@@ -2,22 +2,26 @@ import React from 'react'
 import Navbar from "./Navbar"
 import data from "./data"
 import Card from "./Card"
+
+
 const index = () => {
-    const cards = data.map(item => {
-        return (
-            <Card
-                key={item.id}
-                item={item}
-            />
-        )
-    })  
+  const cards = data.map(item => {
+    return (
+      <Card
+        key={item.id}
+        item={item}
+      />
+    )
+  })
   return (
     <div className="mainbody">
-    <Navbar pagetitle="Properties Page"/>
+      <Navbar
+        pagetitle="Properties Page"
+      />
 
-    <section className="cards-list">
-                {cards}
-            </section>
+      <section className="cards-list">
+        {cards}
+      </section>
     </div>
   )
 }
