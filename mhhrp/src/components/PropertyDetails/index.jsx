b90f46f60e42e15c1ba834a9e5ef4fc91bef0413
Vedant09 <vedantvy98@gmail.com>
2024-02-04 19:49:25 -0500

    add details page

--- mhhrp/src/components/PropertyDetails/index.jsx
+++ mhhrp/src/components/PropertyDetails/index.jsx
@@ -1,30 +1,52 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
 import OutlinedButton from '../common/Button';
 import ImageCarousel from './ImageCarousel';
-import PropertyContent from './PropertyContent'
+import PropertyContent from './PropertyContent';
+import { useLocation } from 'react-router-dom';
+import { getPropertyDetailsById } from "../../api/properties";
 
 
 const PropertyDetails = () => {
 
+    let location = useLocation();
+    const propertyId = location?.state?.id;
+    const [propertyDetail, setpropertyDetail] = useState([]);
+    const [error, setError] = useState("");
+
+    const getDetailsData = async () => {
+        try {
+            setError("");
+            const resData = await getPropertyDetailsById(propertyId);
+            setpropertyDetail(resData?.data?.data);
+        } catch (error) {
+            console.log("hi")
+            setError(error?.message);
+        }
+    };
+    console.log(propertyDetail)
+    useEffect(() => {
+        getDetailsData();
+    }, []);
+
     return (
         <div className='property-details-component'>
             <div className='property-images'>
-                <ImageCarousel/>
+                <ImageCarousel />
             </div>
             <div className='property-info'>
                 <div className='property-address'>
-                    <h1 className='header'>Carriagepark Fairfax</h1>
-                    <p className='content'>4831 Carriagepark Rd, Fairfax, VA 22030</p>
+                    <h1 className='header'>{propertyDetail.name}</h1>
+                    <p className='content'>{propertyDetail.address}</p>
                 </div>
                 <div className='property-content'>
-                    <PropertyContent/>
+                    <PropertyContent data={propertyDetail}/>
                 </div>
                 <div className='button-container'>
-                <OutlinedButton btnTitle='Apply' className='apply-button'/>
+                    <OutlinedButton btnTitle='Apply' className='apply-button' />
                 </div>
             </div>
         </div>
-        
+
     );
 };
 
